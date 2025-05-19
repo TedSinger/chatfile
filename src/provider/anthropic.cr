a95618f5915f80d2fe535cfa6b4441b59aa6129f
Ted Singer <github@fernmyth.net>
2025-05-18 23:40:55 -0400

    ugly but working tool use

--- src/provider/anthropic.cr
+++ src/provider/anthropic.cr
@@ -15,6 +15,8 @@ module Provider::Anthropic
     {"system", "prompt", String},
     {"temperature", "temperature", Float64},
     {"stop_sequences", "stop_sequences", JSON::Any},
+    {"top_k", "top_k", Int32},
+    {"top_p", "top_p", Float64},
   ]
 
   class Completer < Completer
@@ -49,15 +51,27 @@ module Provider::Anthropic
       conversation_body = JSON.build do |json|
         json.object do
           persona.enrich_json(json, SIMPLE_PARAMS)
-          json.field("thinking", thinking)
           json.field("messages",
             chat.conversation.map { |role, content|
               {"role" => Anthropic.generic_role_to_anthropic_role(role), "content" => content}
             }
           )
           json.field("stream", true)
+          if chat.response_format
+            json.field "tool_choice" do
+              json.object do
+                json.field "type", "tool"
+                json.field "name", "json_schema"
+              end
+            end
+            json.field("tools", [
+              Anthropic.generic_json_schema_to_anthropic_response_format(chat.response_format.not_nil!)
+            ])
+          end
+          json.field("thinking", thinking) if chat.response_format.nil?
         end
       end
+      puts conversation_body
 
       client.post("/v1/messages", headers, conversation_body) do |response|
         if response.status_code == 200
@@ -80,6 +94,37 @@ module Provider::Anthropic
     end
   end
 
+  def self.generic_json_schema_to_anthropic_response_format(json_schema : JSON::Any) : JSON::Any
+    # [
+    #   {
+    #     "name": "get_stock_price",
+    #     "description": "Get the current stock price for a given ticker symbol.",
+    #     "input_schema": {
+    #       "type": "object",
+    #       "properties": {
+    #         "ticker": {
+    #           "type": "string",
+    #           "description": "The stock ticker symbol, e.g. AAPL for Apple Inc."
+    #         }
+    #       },
+    #       "required": ["ticker"]
+    #     }
+    #   }
+    # ]
+    if json_schema.dig?("type") == "object"
+      JSON.parse(JSON.build do |json|
+        json.object do
+          json.field("name", "json_schema")
+          json.field("description", "")
+          json.field("input_schema", json_schema)
+        end
+      end)
+    elsif json_schema.dig?("input_schema", "type") == "object"
+      return json_schema
+    else
+      raise "Don't know how to convert JSON schema to Anthropic response format: #{json_schema}"
+    end
+  end
   class EventStream
     include Iterator(String)
 
@@ -150,6 +195,8 @@ module Provider::Anthropic
         elsif delta = json.dig?("delta", "thinking")
           return delta.as_s
         elsif delta = json.dig?("content_block", "thinking")
+          return delta.as_s
+        elsif delta = json.dig?("delta", "partial_json")
           return delta.as_s
         else
           return nil
